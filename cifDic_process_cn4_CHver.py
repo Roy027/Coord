5d35acd4b02066a16f2fad3610c860eb1b7caa0f
baka36 <47176645+baka36@users.noreply.github.com>
2021-05-18 00:38:48 +0800

    Add files via upload

--- cifDic_process_cn4_CHver.py
+++ cifDic_process_cn4_CHver.py
@@ -8,6 +8,7 @@ from math import sqrt
 from math import comb
 from math import floor
 
+COORDno = 4.0
 
 # Custom decimal floor function found online (since python does not have such function)
 # Returns a value rounded down to a specific number of decimal places.
@@ -20,7 +21,6 @@ def round_decimals_down(number:float, decimals:int=2):
         return floor(number)
 
     factor = 10 ** decimals
-    print(str(number) + " rounded to " + str(floor(number * factor) / factor))
     return floor(number * factor) / factor
 
 # Return the distinct normal vector of planes given by the coordinates from cifDic
@@ -41,14 +41,14 @@ def CoordinatesToPlanes(xyz):
             n_v1 = v1 / sqrt(v1[0]**2 + v1[1]**2 + v1[2]**2)
             n_v2 = v2 / sqrt(v2[0]**2 + v2[1]**2 + v2[2]**2)
             vn = np.cross(n_v1,n_v2)
-            print(sqrt(vn[0]**2 + vn[1]**2 + vn[2]**2))
+            # print(sqrt(vn[0]**2 + vn[1]**2 + vn[2]**2))
             # Check if cross product yields value x or higher.
             # If the product yields lower than x means it's not a plane.
             # Currently it's sin(175deg)=0.08715574274
             if sqrt(vn[0]**2 + vn[1]**2 + vn[2]**2) >= 0.08715574274 :
                 res.append(vn)
-            else:
-                print("OUT!")
+            # else:
+            #     print("OUT!")
     return res
 
 def PlanesToProducts(Planes):
@@ -70,19 +70,36 @@ def PlanesToProducts(Planes):
 
 # %%
 def resultForDots(cif):
-    cn_num = 6.0
+    cn_num = COORDno
     max_angle = comb(int(cn_num),2)
     max_plane = comb(max_angle,2)
 
     res = [cif]
-    with open("./cifDic_0413.json") as json_file:
+    # with open("./cifDic_0413.json") as json_file:
+    #     cifDic = json.load(json_file)
+    #     if cif in cifDic:
+    #         for site in cifDic[cif]['Sites']:
+    #             if len(cifDic[cif]['Sites'][site]['Angles']) > max_angle: continue
+    #             if float(cifDic[cif]['Sites'][site]['CN']) != cn_num: continue
+    #             xyz = cifDic[cif]['Sites'][site]["Coordinates"]
+    #             angles = cifDic[cif]['Sites'][site]["Angles"]
+    #             planes = CoordinatesToPlanes(xyz)
+    #             dots = PlanesToProducts(planes)
+    #             # Fill in the rest of the blanks as -1 if dot product is unavailable
+    #             # Due to a lack of planes.
+    #             if len(dots) != max_plane:
+    #                 for i in range(max_plane - len(dots)):
+    #                     dots.append(-1)
+    #             res.extend([site,angles,dots])
+
+    with open("./Na_Y2020_cifDic.json") as json_file:
         cifDic = json.load(json_file)
         if cif in cifDic:
-            print(cif)
             for site in cifDic[cif]['Sites']:
                 if len(cifDic[cif]['Sites'][site]['Angles']) > max_angle: continue
                 if float(cifDic[cif]['Sites'][site]['CN']) != cn_num: continue
                 xyz = cifDic[cif]['Sites'][site]["Coordinates"]
+                angles = cifDic[cif]['Sites'][site]["Angles"]
                 planes = CoordinatesToPlanes(xyz)
                 dots = PlanesToProducts(planes)
                 # Fill in the rest of the blanks as -1 if dot product is unavailable
@@ -90,56 +107,45 @@ def resultForDots(cif):
                 if len(dots) != max_plane:
                     for i in range(max_plane - len(dots)):
                         dots.append(-1)
-                #insert stuff here
-                res.extend([site,dots])
-
-    with open("./Na_Y2020_cifDic.json") as json_file:
-        cifDic = json.load(json_file)
-        if cif in cifDic:
-            print(cif)
-            for site in cifDic[cif]['Sites']:
-                if len(cifDic[cif]['Sites'][site]['Angles']) > max_angle: continue
-                if float(cifDic[cif]['Sites'][site]['CN']) != cn_num: continue
-                xyz = cifDic[cif]['Sites'][site]["Coordinates"]
-                planes = CoordinatesToPlanes(xyz)
-                dots = PlanesToProducts(planes)
-                # Fill in the rest of the blanks with the value "-1" if dot product is unavailable
-                # Due to a lack of planes.
-                if len(dots) != max_plane:
-                    for i in range(max_plane - len(dots)):
-                        dots.append(-1)
-                res.extend([site,dots])
+                res.extend([site,angles,dots])
     return res
 # %%
-h_pl = ['250795.cif','258369.cif','67005.cif']
-h_py = ['200026.cif','258376.cif','258378.cif','37304.cif']
-Oct = ['164019.cif','186330.cif','186363.cif','187230.cif','237851.cif']
-tri_p = ['150911.cif','151938.cif','153305.cif','196278.cif','405133.cif']
-some = ['240263.cif','251666.cif','429556.cif']
-# %%
 import csv
+
+# Get a list of all the .cif files with the correct CN
 dotResult=[]
-dotResult.append(['Hexagonal Planar',h_pl])
-dotResult.append(['Hexagonal Pyrimidal',h_py])
-dotResult.append(['Octahedral',Oct])
-dotResult.append(['Trigonal Prismatic',tri_p])
-dotResult.append(['Others',some])
+with open("./cifDic_0413.json") as json_file:
+    cifDic = json.load(json_file)
+    for cif in cifDic:
+        for site in cifDic[cif]['Sites']:
+            if float(cifDic[cif]['Sites'][site]['CN']) != COORDno: continue
+            dotResult.append(cif)
+
+# Convert to and fro dictionary and list to remove duplicate entries
+theList = list(dict.fromkeys(dotResult))
 
 # Writing data into csv file.
-with open('.\dotResult_1.csv',mode='w') as dotfile:
+# "towrite" variable contains the information to be written into csv file
+# The variable contains "row" and "siteinfo"
+# "row" contains .cif file name kind of like the label of the row
+# "siteinfo" contains the site name as well as its dot products
+with open('.\CN4stuff.csv',mode='w') as dotfile:
     file_writer = csv.writer(dotfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    for shape in dotResult:
-        for cif in shape[1]:
-            functionResults = resultForDots(cif)
-            row = [shape[0]]
-            row.append(functionResults[0])
-            l = len(functionResults)
-            for i in range(l//2):
-                siteinfo = [functionResults[i*2+1],functionResults[i*2+2]]
-                towrite = []
-                towrite.append(row)
-                towrite.extend(siteinfo)
-                file_writer.writerow(towrite)
+    for cif in theList:
+        functionResults = resultForDots(cif)
+        # Put in .cif file name
+        row = [cif]
+        # Put in the dot products
+        row.append(functionResults[0])
+        l = len(functionResults)
+        # The "3" displays how many parameters to be added into the .csv file per site
+        # In this case the 3 parameteres are site name, angles and dot product lists.
+        for i in range(l//3):
+            siteinfo = [functionResults[i*3+1],functionResults[i*3+2],functionResults[i*3+3]]
+            towrite = []
+            towrite.append(row)
+            towrite.extend(siteinfo)
+            file_writer.writerow(towrite)
 
 # %%
 # a = np.array([[1,2,3],[2,3,4]])
